f9975f3f2fea491ad21f8f325506ecc48c1714e5
Jonathan Ventura <jonathanlventura3@gmail.com>
2023-04-11 22:42:57 -0300

    primeiro commit, iniciando projeto Make your burger

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <navbar
+  :logo="logo_src"
+  :alt="app_name"
+  />
+  <router-view />
+  <footerbar/>
 </template>
 
+<script>
+import Navbar from '@/components/Navbar.vue'
+import Footerbar from '@/components/Footerbar.vue'
+
+export default {
+  components:{
+    Navbar,
+    Footerbar
+  },
+  data(){
+    return{
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger"
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;900&display=swap');
+
+*{
+  font-family: 'Roboto', sans-serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.main-container{
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1{
   text-align: center;
-  color: #2c3e50;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <banner/>
+  <div class="main-container">
+    <h1>Monte seu burger</h1>
+    <burger-form/>
+  </div>
+</template>
+
+<script>
+import Banner from '@/components/Banner.vue';
+import BurgerForm from '@/components/BuegerForm.vue'
+
+export default {
+  name: "HomeView",
+  components: {
+    Banner,
+    BurgerForm
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
