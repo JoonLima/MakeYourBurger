d1287b0d0a779da71dfd1bf4a86d8bc01cd88eb9
Jonathan Ventura <jonathanlventura3@gmail.com>
2023-07-17 21:32:54 -0300

    ajuste das urls da api

--- src/components/BuegerForm.vue
+++ src/components/BuegerForm.vue
@@ -1,190 +1,206 @@
 <template>
-    <div>
-        <div class="mensagem">
-            <mensagem-flutuante :msg="msg" v-show="msg"/>
-        </div>
-        <div>
-            <form id="burger-form" @submit="createBurger">
-                <div class="input-container">
-                    <label for="nome">Nome do cliente:</label>
-                    <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite seu nome">
-                </div>
-                <div class="input-container">
-                    <label for="pao">Escolha o pão:</label>
-                    <select name="pao" id="pao" v-model="pao">
-                        <option value="">Selecione seu pão</option>
-                        <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">
-                            {{pao.tipo}}
-                        </option>
-                    </select>
-                </div>
-                <div class="input-container">
-                    <label for="carne">Escolha sua carne:</label>
-                    <select name="carne" id="carne" v-model="carne">
-                        <option value="">Selecione sua carne</option>
-                        <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">
-                            {{ carne.tipo }}
-                        </option>
-                    </select>
-                </div>
-                <div id="opcionais-container" class="input-container">
-                    <label id="opcionais-title" for="optionais">Selecione os opcionais:</label>
-                    <div v-for="opcional in opcionaisdata" :key="opcional.id" class="checkbox-container">
-                        <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
-                        <span>{{opcional.tipo}}</span>
-                    </div>
-                </div>
-                <div class="input-container">
-                    <input type="submit" class="submit-btn" value="Criar meu Burger">
-                </div>
-            </form>
-        </div>
+  <div>
+    <div class="mensagem">
+      <mensagem-flutuante :msg="msg" v-show="msg" />
     </div>
+    <div>
+      <form id="burger-form" @submit="createBurger">
+        <div class="input-container">
+          <label for="nome">Nome do cliente:</label>
+          <input
+            type="text"
+            id="nome"
+            name="nome"
+            v-model="nome"
+            placeholder="Digite seu nome"
+          />
+        </div>
+        <div class="input-container">
+          <label for="pao">Escolha o pão:</label>
+          <select name="pao" id="pao" v-model="pao">
+            <option value="">Selecione seu pão</option>
+            <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">
+              {{ pao.tipo }}
+            </option>
+          </select>
+        </div>
+        <div class="input-container">
+          <label for="carne">Escolha sua carne:</label>
+          <select name="carne" id="carne" v-model="carne">
+            <option value="">Selecione sua carne</option>
+            <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">
+              {{ carne.tipo }}
+            </option>
+          </select>
+        </div>
+        <div id="opcionais-container" class="input-container">
+          <label id="opcionais-title" for="optionais"
+            >Selecione os opcionais:</label
+          >
+          <div
+            v-for="opcional in opcionaisdata"
+            :key="opcional.id"
+            class="checkbox-container"
+          >
+            <input
+              type="checkbox"
+              name="opcionais"
+              v-model="opcionais"
+              :value="opcional.tipo"
+            />
+            <span>{{ opcional.tipo }}</span>
+          </div>
+        </div>
+        <div class="input-container">
+          <input type="submit" class="submit-btn" value="Criar meu Burger" />
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-import MensagemFlutuante from './MensagemFlutuante.vue';
+import MensagemFlutuante from "./MensagemFlutuante.vue";
 
 export default {
-    name:'BurgerForm',
-    components:{
-        MensagemFlutuante
-    },
-    data(){
-        return{
-           paes: null,
-           carnes: null,
-           opcionaisdata: null,
-           nome: null,
-           pao: null,
-           carne: null,
-           opcionais: [],
-           msg: null
-        }
-    },
+  name: "BurgerForm",
+  components: {
+    MensagemFlutuante,
+  },
+  data() {
+    return {
+      paes: null,
+      carnes: null,
+      opcionaisdata: null,
+      nome: null,
+      pao: null,
+      carne: null,
+      opcionais: [],
+      msg: null,
+    };
+  },
 
-    methods:{
-        async getIngredientes(){
-            const req = await fetch("http://localhost:3000/ingredientes");
-            const data = await req.json();
+  methods: {
+    async getIngredientes() {
+      const req = await fetch("https://make-burger-db.vercel.app/ingredientes");
+      const data = await req.json();
 
-            this.paes = data.paes;
-            this.carnes = data.carnes;
-            this.opcionaisdata = data.opcionais;
-        },
-
-        async createBurger(e){
-            e.preventDefault();
-
-            const data = {
-                nome: this.nome,
-                carne: this.carne,
-                pao: this.pao,
-                opcionais: Array.from(this.opcionais),
-                status: "Solicitado"
-            }
-
-            const dataJson = JSON.stringify(data);
-
-            const req = await fetch("http://localhost:3000/burgers", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: dataJson
-            });
-
-            const res = await req.json();
-            
-            //mensagem de sistema.
-            this.msg = `Pedido Nº ${res.id} realizado.`
-
-            //limpar mensagem
-            setTimeout(() => this.msg = '', 3000)
-
-            //limpar os campos
-            this.nome = '';
-            this.carne = '';
-            this.pao = '';
-            this.opcionais = '';
-        }
+      this.paes = data.paes;
+      this.carnes = data.carnes;
+      this.opcionaisdata = data.opcionais;
     },
 
-    mounted(){
-        this.getIngredientes();
-    }
-}
+    async createBurger(e) {
+      e.preventDefault();
+
+      const data = {
+        nome: this.nome,
+        carne: this.carne,
+        pao: this.pao,
+        opcionais: Array.from(this.opcionais),
+        status: "Solicitado",
+      };
+
+      const dataJson = JSON.stringify(data);
+
+      const req = await fetch("https://make-burger-db.vercel.app/burgers", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: dataJson,
+      });
+
+      const res = await req.json();
+
+      //mensagem de sistema.
+      this.msg = `Pedido Nº ${res.id} realizado.`;
+
+      //limpar mensagem
+      setTimeout(() => (this.msg = ""), 3000);
+
+      //limpar os campos
+      this.nome = "";
+      this.carne = "";
+      this.pao = "";
+      this.opcionais = "";
+    },
+  },
+
+  mounted() {
+    this.getIngredientes();
+  },
+};
 </script>
 
 <style scoped>
-
-.mensagem{
-    height: 50px;
+.mensagem {
+  height: 50px;
 }
 
-#burger-form{
-    max-width: 400px;
-    margin: 0 auto;
+#burger-form {
+  max-width: 400px;
+  margin: 0 auto;
 }
 
-.input-container{
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 20px;
+.input-container {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
 }
 
-label{
-    font-weight: bold;
-    margin-bottom: 15px;
-    color: #222;
-    padding: 5px 10px;
-    border-left: 4px solid #fcba03;
+label {
+  font-weight: bold;
+  margin-bottom: 15px;
+  color: #222;
+  padding: 5px 10px;
+  border-left: 4px solid #fcba03;
 }
 
-input,select{
-    padding: 5px 10px;
-    width: 100%;
+input,
+select {
+  padding: 5px 10px;
+  width: 100%;
 }
 
-#opcionais-container{
-    flex-direction: row;
-    flex-wrap: wrap;
+#opcionais-container {
+  flex-direction: row;
+  flex-wrap: wrap;
 }
 
-#opcionais-title{
-    width: 100%;
+#opcionais-title {
+  width: 100%;
 }
 
-.checkbox-container{
-    display: flex;
-    align-items: flex-start;
-    width: 50%;
-    margin-bottom: 20px;
+.checkbox-container {
+  display: flex;
+  align-items: flex-start;
+  width: 50%;
+  margin-bottom: 20px;
 }
 
 .checkbox-container span,
-.checkbox-container input{
-    width: auto;
+.checkbox-container input {
+  width: auto;
 }
 
-.checkbox-container span{
-    margin-left: 6px;
-    font-weight: bold;
+.checkbox-container span {
+  margin-left: 6px;
+  font-weight: bold;
 }
 
-.submit-btn{
-    background-color: #222;
-    color: #fcba03;
-    font-weight: bold;
-    border: 2px solid #222;
-    padding: 10px;
-    font-size: 16px;
-    margin: 0 auto;
-    cursor: pointer;
-    transition: .5s;
+.submit-btn {
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
 }
 
-.submit-btn:hover{
-    background-color: transparent;
-    color: #222;
+.submit-btn:hover {
+  background-color: transparent;
+  color: #222;
 }
-
 </style>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,174 +1,189 @@
 <template>
-    <div id="burger-table">
-        <div class="mensagem">
-            <mensagem-flutuante :msg="msg" v-show="msg"/>
-        </div>
-        <div id="burger-table-heading">
-            <div class="order id">#:</div>
-            <div>Cliente:</div>
-            <div>Pão:</div>
-            <div>Carne:</div>
-            <div>Opcionais:</div>
-            <div>Ações:</div>
-        </div>
-        <div id="burger-table-rows">
-            <div v-for="burger in burgers" :key="burger.id" class="burger-table-row">
-                <div class="order-number">{{ burger.id  }}</div>
-                <div>{{burger.nome}}</div>
-                <div>{{ burger.pao }}</div>
-                <div>{{ burger.carne }}</div>
-                <div>
-                    <ul>
-                        <li v-for="(opcional, index) in burger.opcionais" :key="index">
-                            {{ opcional }}
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <select name="status" class="status" @change="updateBurger($event, burger.id)">
-                        <option value="">Selecione</option>
-                        <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">{{ s.tipo }}</option>
-                    </select>
-                    <button @click="deleteBurger(burger.id)" class="delete-btn">Cancelar</button>
-                </div>
-            </div>
-        </div>
+  <div id="burger-table">
+    <div class="mensagem">
+      <mensagem-flutuante :msg="msg" v-show="msg" />
     </div>
+    <div id="burger-table-heading">
+      <div class="order id">#:</div>
+      <div>Cliente:</div>
+      <div>Pão:</div>
+      <div>Carne:</div>
+      <div>Opcionais:</div>
+      <div>Ações:</div>
+    </div>
+    <div id="burger-table-rows">
+      <div v-for="burger in burgers" :key="burger.id" class="burger-table-row">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.nome }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.carne }}</div>
+        <div>
+          <ul>
+            <li v-for="(opcional, index) in burger.opcionais" :key="index">
+              {{ opcional }}
+            </li>
+          </ul>
+        </div>
+        <div>
+          <select
+            name="status"
+            class="status"
+            @change="updateBurger($event, burger.id)"
+          >
+            <option value="">Selecione</option>
+            <option
+              v-for="s in status"
+              :key="s.id"
+              :value="s.tipo"
+              :selected="burger.status == s.tipo"
+            >
+              {{ s.tipo }}
+            </option>
+          </select>
+          <button @click="deleteBurger(burger.id)" class="delete-btn">
+            Cancelar
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import MensagemFlutuante from './MensagemFlutuante.vue';
+import MensagemFlutuante from "./MensagemFlutuante.vue";
 
 export default {
-    name: 'Dashboard',
-    components:{
-        MensagemFlutuante
+  name: "Dashboard",
+  components: {
+    MensagemFlutuante,
+  },
+  data() {
+    return {
+      burgers: null,
+      burger_id: null,
+      status: [],
+      msg: null,
+    };
+  },
+
+  methods: {
+    async getPedidos() {
+      const req = await fetch("https://make-burger-db.vercel.app/burgers");
+      const data = await req.json();
+
+      this.burgers = data;
+      this.getStatus();
     },
-    data(){
-        return{
-            burgers: null,
-            burger_id: null,
-            status: [],
-            msg: null
+
+    async getStatus() {
+      const req = await fetch("https://make-burger-db.vercel.app/status");
+      const data = await req.json();
+
+      this.status = data;
+    },
+
+    async deleteBurger(id) {
+      const req = await fetch(
+        `https://make-burger-db.vercel.app/burgers/${id}`,
+        {
+          method: "DELETE",
         }
+      );
+
+      const res = await req.json();
+      this.msg = `Pedido removido.`;
+
+      //limpar mensagem
+      setTimeout(() => (this.msg = ""), 3000);
+
+      this.getPedidos();
     },
 
-    methods:{
-        async getPedidos(){
+    async updateBurger(event, id) {
+      const option = event.target.value;
 
-            const req = await fetch("http://localhost:3000/burgers");
-            const data = await req.json();
+      const dataJson = JSON.stringify({ status: option });
 
-            this.burgers = data;
-            this.getStatus();
-        },
-
-        async getStatus(){
-            const req = await fetch("http://localhost:3000/status");
-            const data = await req.json();
-
-            this.status = data;
-        },
-
-        async deleteBurger(id){
-            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
-                method: "DELETE"
-            })
-
-            const res = await req.json()
-            this.msg = `Pedido removido.`
-
-            //limpar mensagem
-            setTimeout(() => this.msg = '', 3000)
-
-            this.getPedidos();
-        },
-
-        async updateBurger(event, id){
-
-            const option = event.target.value;
-
-            const dataJson = JSON.stringify({ status: option })
-
-            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
-                method: "PATCH",
-                headers: { "Content-Type": "application/json" },
-                body: dataJson
-            });
-
-            const res = await req.json();
-
-            this.msg = `Pedido Nº ${res.id} atualizado para o status ${res.status}`
-            setTimeout(() => this.msg = '', 3000)
+      const req = await fetch(
+        `https://make-burger-db.vercel.app/burgers/${id}`,
+        {
+          method: "PATCH",
+          headers: { "Content-Type": "application/json" },
+          body: dataJson,
         }
-    },
+      );
 
-    mounted(){
-        this.getPedidos()
-    }
-}
+      const res = await req.json();
+
+      this.msg = `Pedido Nº ${res.id} atualizado para o status ${res.status}`;
+      setTimeout(() => (this.msg = ""), 3000);
+    },
+  },
+
+  mounted() {
+    this.getPedidos();
+  },
+};
 </script>
 
 <style scoped>
-
-.mensagem{
-    height: 50px;
+.mensagem {
+  height: 50px;
 }
 
-#burger-table{
-    max-width: 1200px;
-    margin: 0 auto;
+#burger-table {
+  max-width: 1200px;
+  margin: 0 auto;
 }
 
 #burger-table-heading,
 #burger-table-rows,
-.burger-table-row{
-    display: flex;
-    flex-wrap: wrap;
+.burger-table-row {
+  display: flex;
+  flex-wrap: wrap;
 }
 
-#burger-table-heading{
-    font-weight: bold;
-    padding: 12px;
-    border-bottom: 3px solid #333;
+#burger-table-heading {
+  font-weight: bold;
+  padding: 12px;
+  border-bottom: 3px solid #333;
 }
 
 #burger-table-heading div,
 .burger-table-row div {
-    width: 19%;
+  width: 19%;
 }
 
-.burger-table-row{
-    width: 100%;
-    padding: 12px;
-    border-bottom: 1px solid #ccc;
+.burger-table-row {
+  width: 100%;
+  padding: 12px;
+  border-bottom: 1px solid #ccc;
 }
 
 #burger-table-heading .order-id,
-.burger-table-row .order-number{
-    width: 5%;
+.burger-table-row .order-number {
+  width: 5%;
 }
 
-select{
-    padding: 12px 6px;
-    margin-right: 12px;
+select {
+  padding: 12px 6px;
+  margin-right: 12px;
 }
 
-.delete-btn{
-    background-color: #222;
-    color: #fcba03;
-    font-weight: bold;
-    border: 2px solid #222;
-    padding: 10px;
-    font-size: 16px;
-    margin: 0 auto;
-    cursor: pointer;
-    transition: .5s;
+.delete-btn {
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
 }
 
-.delete-btn:hover{
-    background-color: transparent;
-    color: #222;
+.delete-btn:hover {
+  background-color: transparent;
+  color: #222;
 }
-
 </style>
